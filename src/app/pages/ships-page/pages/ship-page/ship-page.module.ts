f8f0681af62cfbd712249b09c78cdad440d3a23c
Eremeow138 <eremeow138@yandex.ru>
2022-04-04 10:49:49 +0800

    feat: implement markup for ship-page

--- src/app/pages/ships-page/pages/ship-page/ship-page.module.ts
+++ src/app/pages/ships-page/pages/ship-page/ship-page.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShipPageComponent } from './components/ship-page/ship-page.component';
 import { ShipPageRoutingModule } from './ship-page-routing.module';
+import { CommonsModule } from 'src/app/commons/commons.module';
 
 const COMPONENTS = [ShipPageComponent];
 
 @NgModule({
   declarations: [...COMPONENTS],
-  imports: [CommonModule, ShipPageRoutingModule],
+  imports: [CommonModule, CommonsModule, ShipPageRoutingModule],
   exports: [...COMPONENTS],
 })
 export class ShipPageModule {}
