c9f91981d074f3c6a8c0c2238405c4fc2fa6e006
Eremeow138 <eremeow138@yandex.ru>
2022-04-04 22:18:25 +0800

    fix: add logic to ship page

--- src/app/pages/ships-page/pages/ship-page/components/ship-page/ship-page.component.ts
+++ src/app/pages/ships-page/pages/ship-page/components/ship-page/ship-page.component.ts
@@ -1,12 +1,86 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { RouterPath } from 'src/app/commons/enums/router-path.enum';
+import { ITableRow } from 'src/app/commons/interfaces/table-row.interface';
+import { IShipFull } from 'src/app/pages/ships-page/ships/interfaces/ship-full.interface';
+import { ShipsService } from 'src/app/pages/ships-page/ships/services/ships.service';
 
 @Component({
   selector: 'app-ship-page',
   templateUrl: './ship-page.component.html',
   styleUrls: ['./ship-page.component.scss'],
 })
-export class ShipPageComponent implements OnInit {
-  constructor() {}
+export class ShipPageComponent implements OnInit, OnDestroy {
+  public ship: IShipFull | null = null;
 
-  ngOnInit(): void {}
+  public tableRows: ITableRow[] | null = null;
+
+  private unsubscribe$ = new Subject<void>();
+
+  constructor(
+    private shipsService: ShipsService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {}
+
+  public ngOnInit(): void {
+    this.subscribeToGettingShip();
+  }
+
+  public ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  public getMissions(): string {
+    if (!this.ship) {
+      return '';
+    }
+    return this.ship?.missions.reduce((string, currentMission, index, array) => {
+      const returns = string + currentMission.name;
+      if (index === array.length - 1) {
+        return returns;
+      }
+      return returns + ', ';
+    }, '');
+  }
+
+  public navigateToShipsListPage(): void {
+    this.router.navigateByUrl(`/${RouterPath.ShipsListPage}`);
+  }
+
+  private subscribeToGettingShip(): void {
+    this.shipsService
+      .getShipById(this.route.snapshot.params.id)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((ship) => {
+        this.ship = ship;
+        this.tableRows = [
+          {
+            cells: [
+              'Тип',
+              ship.type ? ship.type : 'No data',
+              'Порт',
+              ship.home_port ? ship.home_port : 'No data',
+            ],
+          },
+          {
+            cells: [
+              'Вес',
+              `${ship.weight_kg ? ship.weight_kg : 'No data'}`,
+              'Год',
+              `${ship.year_built ? ship.year_built : 'No data'}`,
+            ],
+          },
+          { cells: ['Миссии'] },
+          {
+            cells: [this.getMissions() ? this.getMissions() : 'No data'],
+            colspan: 4,
+            cellClasses: 'table__cell_blue table__cell_only-bottom-padding',
+          },
+        ];
+      });
+  }
 }
--- src/app/pages/ships-page/ships/services/ships.service.ts
+++ src/app/pages/ships-page/ships/services/ships.service.ts
@@ -4,6 +4,8 @@ import { Apollo, ApolloBase, gql } from 'apollo-angular';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { apolloSettings } from 'src/settings/settings';
+import { IShipFullResponse } from '../interfaces/ship-full-response';
+import { IShipFull } from '../interfaces/ship-full.interface';
 import { IShipResponse } from '../interfaces/ship-response';
 import { IShip } from '../interfaces/ship.interface';
 
@@ -24,6 +26,22 @@ export class ShipsService {
     }
   `;
 
+  private getShipQuery = gql`
+    query ShipById($id: ID!) {
+      ship(id: $id) {
+        id
+        home_port
+        name
+        type
+        weight_kg
+        year_built
+        missions {
+          name
+        }
+      }
+    }
+  `;
+
   constructor(private apolloProvider: Apollo) {
     this.apollo = this.apolloProvider.use(apolloSettings.clientNames.spaceXClient);
   }
@@ -38,5 +56,16 @@ export class ShipsService {
         },
       })
       .pipe(map((app: ApolloQueryResult<IShipResponse>) => app.data.ships));
+  }
+
+  getShipById(id: string): Observable<IShipFull> {
+    return this.apollo
+      .query<IShipFullResponse>({
+        query: this.getShipQuery,
+        variables: {
+          id,
+        },
+      })
+      .pipe(map((app: ApolloQueryResult<IShipFullResponse>) => app.data.ship));
   }
 }
