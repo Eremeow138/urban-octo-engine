317920e102c6b0ca64abec7081f05f5bf9efcdd4
Eremeow138 <eremeow138@yandex.ru>
2022-04-05 12:09:08 +0800

    feat: add ships-filters-form markup

---
+++ src/app/pages/ships-page/pages/ships-list-page/components/ships-filters-form/ships-filters-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ships-filters-form',
+  templateUrl: './ships-filters-form.component.html',
+  styleUrls: ['./ships-filters-form.component.scss'],
+})
+export class ShipsFiltersFormComponent {}
--- src/app/pages/ships-page/pages/ships-list-page/ships-list-page.module.ts
+++ src/app/pages/ships-page/pages/ships-list-page/ships-list-page.module.ts
@@ -5,11 +5,12 @@ import { ShipsListPageComponent } from './components/ships-list-page.component';
 import { ShipsListComponent } from './components/ships-list/ships-list.component';
 import { ShipCardComponent } from './components/ships-list/ship-card/ship-card.component';
 import { CommonsModule } from 'src/app/commons/commons.module';
+import { ShipsFiltersFormComponent } from './components/ships-filters-form/ships-filters-form.component';
 
 const COMPONENTS = [ShipsListPageComponent];
 
 @NgModule({
-  declarations: [...COMPONENTS, ShipsListComponent, ShipCardComponent],
+  declarations: [...COMPONENTS, ShipsListComponent, ShipCardComponent, ShipsFiltersFormComponent],
   imports: [CommonModule, CommonsModule, ShipListPageRoutingModule],
   exports: [...COMPONENTS],
 })
